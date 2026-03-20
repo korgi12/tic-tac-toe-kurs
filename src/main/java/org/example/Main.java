d6a4b3c6fd4484a012582f6bc2934cadf2127aec
Egor <65020062+korgi12@users.noreply.github.com>
2026-03-20 15:28:16 +0400

    init

--- src/main/java/org/example/Main.java
+++
@@ -1,9 +0,0 @@
-package org.example;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Запускайте приложение через отдельные классы:");
-        System.out.println("  server: org.example.ServerMain");
-        System.out.println("  client: org.example.ClientMain");
-    }
-}
